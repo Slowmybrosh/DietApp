4eff61a61ddfde280689a7cbc5a4a0506e6f9219
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-11-14 11:35:20 +0100

    Fixed error when assigning number type to undefined type in constructor

---
+++ src/ValorNutricional.ts
@@ -0,0 +1,89 @@
+class ValorNutricional { 
+    
+    private id:string;
+    private kilojulios:number;
+    private kilocalorias:number;
+    private grasa:number;
+    private proteinas:number;
+    private fibra:number;
+    private sal:number;
+    private azucar:number;
+    private sodio:number;
+    private calcio:number;
+    private hierro:number;
+    private fosforo:number;
+    private potasio:number;
+
+    constructor(id: string, kilojulios?: number, kilocalorias?: number, grasa?: number, proteinas?: number,
+        fibra?: number, sal?: number, azucar?: number, sodio?: number, calcio?: number, hierro?: number,
+        fosforo?: number,potasio?: number) {
+
+        if( kilojulios !== undefined && kilocalorias !== undefined && grasa !== undefined && proteinas !== undefined && fibra !== undefined && sal !== undefined && azucar !== undefined && sodio !== undefined && calcio !== undefined && hierro !== undefined && fosforo !== undefined && potasio !== undefined){
+            this.id = id;
+            this.kilojulios = kilojulios;
+            this.kilocalorias = kilocalorias;
+            this.grasa = grasa;
+            this.proteinas = proteinas;
+            this.fibra = fibra;
+            this.sal = sal;
+            this.azucar = azucar;
+            this.sodio = sodio;
+            this.calcio = calcio;
+            this.hierro = hierro;
+            this.fosforo = fosforo;
+            this.potasio = potasio;
+        }
+        else{
+            throw new Error('Parametros del constructor invalidos');
+        }
+    }
+
+    public getKilojulios():number{
+        return this.kilojulios;
+    }
+
+    public getKilocalorias():number{
+        return this.kilocalorias;
+    }
+
+    public getGrasa():number{
+        return this.grasa;
+    }
+
+    public getProteinas():number{
+        return this.proteinas;
+    }
+
+    public getFibra():number{
+        return this.fibra;
+    }
+
+    public getSal():number{
+        return this.sal;
+    }
+
+    public getAzucar():number{
+        return this.azucar;
+    }
+    
+    public getSodio():number{
+        return this.sodio;
+    }
+
+    public getCalcio():number{
+        return this.calcio;
+    }
+
+    public getHierro():number{
+        return this.hierro;
+    }
+
+    public getFosforo():number{
+        return this.fosforo;
+    }
+
+    public getPotasio():number{
+        return this.potasio;
+    }
+
+}
