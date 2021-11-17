b198d23aa9a0f095c8ae24a01636a43033c12726
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-11-17 17:27:40 +0100

    Removed kilocalorias property and changed exception management

--- src/ValorNutricional.ts
+++ src/ValorNutricional.ts
@@ -17,84 +17,84 @@ class ValorNutricional {
         fibra?: number, sal?: number, azucar?: number, sodio?: number, calcio?: number, hierro?: number,
         fosforo?: number,potasio?: number) {
 
-        this.id = id;
+            this.id = id;
         
-        if(kilojulios !== undefined){
-            this.kilojulios = kilojulios;
-        }
-        else{
-            throw new Error('Los kilojulios no pueden estar vacíos');
-        }
-
-        if(grasa !== undefined){
-            this.grasa = grasa;
-        }
-        else{
-            throw new Error('La grasa no puede estar vacía');
-        }
-
-        if(proteinas !== undefined){
-            this.proteinas = proteinas;
-        }
-        else{
-            throw new Error('Las proteinas no pueden estar vacías');
-        }
-
-        if(fibra !== undefined){
-            this.fibra = fibra;
-        }
-        else{
-            throw new Error('La fibra no puede estar vacía');
-        }
-
-        if(sal !== undefined){
-            this.sal = sal;
-        }
-        else{
-            throw new Error('La sal no puede estar vacía');
-        }
-
-        if(azucar !== undefined){
-            this.azucar = azucar;
-        }
-        else{
-            throw new Error('El azucar no puede estar vacío');
-        }
-
-        if(sodio !== undefined){
-            this.sodio = sodio;
-        }
-        else{
-            throw new Error('El sodio no puede estar vacío');
-        }
-
-        if(calcio !== undefined){
-            this.calcio = calcio;
-        }
-        else{
-            throw new Error('El calcio no puede estar vacío');
-        }
-
-        if(hierro !== undefined){
-            this.hierro = hierro;
-        }
-        else{
-            throw new Error('El hierro no puede estar vacío');
-        }
-
-        if(potasio !== undefined){
-            this.potasio = potasio;
-        }
-        else{
-            throw new Error('El potasio no puede estar vacío');
-        }
-
-        if(fosforo !== undefined){
-            this.fosforo = fosforo;
-        }
-        else{
-            throw new Error('El fosforo no puede estar vacío');
-        }
+            if(kilojulios !== undefined){
+                this.kilojulios = kilojulios;
+            }
+            else{
+                throw new Error('Los kilojulios no pueden estar vacíos');
+            }
+    
+            if(grasa !== undefined){
+                this.grasa = grasa;
+            }
+            else{
+                throw new Error('La grasa no puede estar vacía');
+            }
+    
+            if(proteinas !== undefined){
+                this.proteinas = proteinas;
+            }
+            else{
+                throw new Error('Las proteinas no pueden estar vacías');
+            }
+    
+            if(fibra !== undefined){
+                this.fibra = fibra;
+            }
+            else{
+                throw new Error('La fibra no puede estar vacía');
+            }
+    
+            if(sal !== undefined){
+                this.sal = sal;
+            }
+            else{
+                throw new Error('La sal no puede estar vacía');
+            }
+    
+            if(azucar !== undefined){
+                this.azucar = azucar;
+            }
+            else{
+                throw new Error('El azucar no puede estar vacío');
+            }
+    
+            if(sodio !== undefined){
+                this.sodio = sodio;
+            }
+            else{
+                throw new Error('El sodio no puede estar vacío');
+            }
+    
+            if(calcio !== undefined){
+                this.calcio = calcio;
+            }
+            else{
+                throw new Error('El calcio no puede estar vacío');
+            }
+    
+            if(hierro !== undefined){
+                this.hierro = hierro;
+            }
+            else{
+                throw new Error('El hierro no puede estar vacío');
+            }
+    
+            if(potasio !== undefined){
+                this.potasio = potasio;
+            }
+            else{
+                throw new Error('El potasio no puede estar vacío');
+            }
+    
+            if(fosforo !== undefined){
+                this.fosforo = fosforo;
+            }
+            else{
+                throw new Error('El fosforo no puede estar vacío');
+            }
     }
 
     public getKilojulios():number{
