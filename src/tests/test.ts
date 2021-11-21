312fd9513a86c6f77851f1edfa92a653572b60ff
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-11-21 20:01:12 +0100

    Deleted id from ValorNutricional

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -4,33 +4,33 @@ import { ValorNutricional } from "../ValorNutricional";
 describe('Clase ValorNutricional', () => {
     it('Comprobar creación de ValorNutricional', () =>{
         const negativeKilocalorias = () => {
-            new ValorNutricional('aaa', -5)
+            new ValorNutricional(-5)
         };
         expect(negativeKilocalorias).toThrow(Error_VN)
 
         const negativeGrasa = () => {
-            new ValorNutricional('aaa',0,-5)
+            new ValorNutricional(0,-5)
         };
         expect(negativeGrasa).toThrow(Error_VN)
 
         const negativeProteina = () => {
-            new ValorNutricional('aaa',0,0,-5)
+            new ValorNutricional(0,0,-5)
         };
         expect(negativeProteina).toThrow(Error_VN)
 
         const negativefibra = () => {
-            new ValorNutricional('aaa',0,0,-5)
+            new ValorNutricional(0,0,-5)
         };
         expect(negativefibra).toThrow(Error_VN)
 
         const negativeCarbohidratos = () => {
-            new ValorNutricional('aaa',0,0,0,0,-5)
+            new ValorNutricional(0,0,0,0,-5)
         };
         expect(negativeCarbohidratos).toThrow(Error_VN)
     })
 
     let valor: ValorNutricional
-    valor = new ValorNutricional('aaa',2500,168,657,34,300)
+    valor = new ValorNutricional(2500,168,657,34,300)
 
     it('Comprobar getters', () =>{
         expect(valor.getKilocalorias()).toBe(2500)
