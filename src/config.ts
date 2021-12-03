f8ffb52761b14ac916e209e860b502918c8e55b9
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-12-03 16:08:30 +0100

    Añadida la gestion de errores a la clase 'Config'

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import { Etcd3 } from "etcd3";
+import { Error_Config } from "./errores";
 require('dotenv').config({ path:'./config/enviroment.env'})
 
 const client = new Etcd3();
@@ -9,13 +10,7 @@ export class Config {
         if(process.env.LOG_DIR != undefined)
             this.log_dir = process.env.LOG_DIR;
         else
-            throw new Error("LOG_DIR no existe en el fichero variables")
-        // this.log_dir = String(client.get('LOG_DIR').string())
-        // if(this.log_dir == undefined)
-        //     if(process.env.LOG_DIR != undefined){
-        //         this.log_dir = process.env.LOG_DIR;
-        //     }
-        //     //Else throw error en configuración
+            throw new Error_Config("LOG_DIR no existe en el fichero variables")
     }
 
     public getLog_dir():string{
