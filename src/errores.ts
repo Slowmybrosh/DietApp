c090761b43fdaf84027637239963001641bbaf91
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-12-03 16:05:01 +0100

    Creada gestion de errores para la clase 'Config', advances #39

--- src/errores.ts
+++ src/errores.ts
@@ -29,3 +29,16 @@ export class Error_Dieta extends Error{
     }
 
 }
+
+/**
+ * Clase para gestionar los errores en la clase config
+ *  @public
+ */
+export class Error_Config extends Error{
+    /**
+     * Constructor de clase
+     */
+    constructor(message: string){
+        super(message);
+    }
+}
