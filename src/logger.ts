6aa3c21928599b368c314e9ca283938e86683544
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-12-08 23:00:23 +0100

    Added log directory

--- src/logger.ts
+++ src/logger.ts
@@ -4,9 +4,9 @@ import * as fs from 'fs';
 
 const Configuration = new Config()
 
-// if(!fs.existsSync(Configuration.getLog_folder())){
-//     fs.mkdirSync('./logs')
-// }
+if(!fs.existsSync(Configuration.getLog_folder())){
+    fs.mkdirSync('./logs')
+}
 
 const pino = require('pino')
 const dest = pino.destination(Configuration.getLog_dir())
