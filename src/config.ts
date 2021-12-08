06afee9aa46e2e56f4170d43599dcecae1a806db
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-12-08 14:24:48 +0100

    Added LOG_FOLDER to configuration class

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,8 @@ require('dotenv').config({ path:'./config/enviroment.env'})
 const client = new Etcd3();
 
 export class Config {
-    private log_dir: any; 
+    private log_dir: any;
+    private log_folder: any; 
     constructor(){
         (async () => {
             this.log_dir = await client.get('LOG_DIR').string().catch(err =>{});
@@ -16,9 +17,24 @@ export class Config {
         else{
             throw new Error_Config("Hay un problema con el fichero de configuración")
         }
+
+        (async () => {
+            this.log_folder = await client.get('LOG_FOLDER').string().catch(err =>{});
+        })();
+        if(this.log_folder == null && process.env.LOG_FOLDER != undefined){
+            this.log_folder = String(process.env.LOG_FOLDER);
+        }
+        else{
+            throw new Error_Config("Hay un problema con el directorio de configuración")
+        }
+
     }
 
     public getLog_dir():string{
         return this.log_dir;
+    }
+
+    public getLog_folder():string{
+        return this.log_folder
     }
 }
