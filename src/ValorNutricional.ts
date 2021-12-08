538b99aa46ef4c218608cb4e5112241f76884da8
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-12-08 12:59:02 +0100

    advances #39
    Se ha añadido el uso del logger a la clase ValorNutricional

--- src/ValorNutricional.ts
+++ src/ValorNutricional.ts
@@ -1,4 +1,7 @@
 import { Error_VN } from "./errores";
+import { logger } from "./logger";
+
+const child = logger.child({a:'ValorNutricional'})
 
 /**
  * Clase que contiene los valores nutricionales para los alimetos de la dieta
@@ -18,13 +21,15 @@ export class ValorNutricional {
                 this.kilocalorias = kilocalorias;
             }
             else{
-                throw new Error_VN('El valor de kilojulios no ser negativo');
+                child.error("Valor kilocalorias incorrecto");
+                throw new Error_VN('El valor de kilocalorias no ser negativo');
             }
     
             if(grasa >= 0){
                 this.grasa = grasa;
             }
             else{
+                child.error("Valor Grasa incorrecto")
                 throw new Error_VN('El valor de grasa no ser negativo');
             }
     
@@ -32,6 +37,7 @@ export class ValorNutricional {
                 this.proteinas = proteinas;
             }
             else{
+                child.error("Valor proteinas incorrecto")
                 throw new Error_VN('El valor de proteinas no puede ser negativo');
             }
     
@@ -39,6 +45,7 @@ export class ValorNutricional {
                 this.fibra = fibra;
             }
             else{
+                child.error("Valor fibra incorrecto")
                 throw new Error_VN('El valor de fibra no ser negativo');
             }
 
@@ -46,8 +53,11 @@ export class ValorNutricional {
                 this.carbohidratos = carbohidratos;
             }
             else{
+                child.error("Valor carbohidratos incorrecto")
                 throw new Error_VN('El valor de carbohidratos no ser negativo');
             }
+
+            child.info("Creado objeto de la clase ValorNutricional")
     }
 
     public getKilocalorias():number{
