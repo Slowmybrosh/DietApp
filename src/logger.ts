1b8d20b7b73d48e81306260a1c5e3c41bba092f3
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-12-08 14:23:40 +0100

    Starts #42
    Si no existe la carpeta logs en el directorio se crea una nueva

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,13 @@
 import { Config } from "./config";
 import * as fs from 'fs';
 
+
 const Configuration = new Config()
+
+if(!fs.existsSync(Configuration.getLog_folder())){
+    fs.mkdirSync('./logs')
+}
+
 const pino = require('pino')
 const dest = pino.destination(Configuration.getLog_dir())
 export const logger = pino(dest)
