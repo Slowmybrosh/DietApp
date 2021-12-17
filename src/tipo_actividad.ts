7993fa655970e3a52c72f153b60fe9a15437dfdc
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-12-17 12:12:14 +0100

    Changed src directory to MVC

--- src/tipo_actividad.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Clase que contiene los diferentes tipos de actividad
- * @public
- */
-export enum Tipo_actividad{
-    SEDENTARIO, LIGERO, MODERADO, REGULAR, ELITE
-}
--- src/tipo_dieta.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Clase que contiene los diferentes tipos de dieta
- * @public
- */
- export enum Tipo_dieta{
-    EQUILIBRADA, REDUCIR, AUMENTAR
-}
