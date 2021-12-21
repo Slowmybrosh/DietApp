4692b5c0d7f23dd872268c87c17b94a130ab1fd6
Alejandro Olivares del Rey Pierres <a.olivaresdelrey.pierres@gmail.com>
2021-12-21 12:16:32 +0100

    Added Fastify port to config class

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,11 @@ require('dotenv').config({ path:'./config/configuration.env'})
 
 const client = new Etcd3();
 
-export class Config {
+class Config {
     private log_file: any;
-    private log_folder: any; 
+    private log_folder: any;
+    private fastify_port: any;
+
     constructor(){
         (async () => {
             this.log_file = await client.get('LOG_FILE').string().catch(err =>{});
@@ -28,6 +30,16 @@ export class Config {
             this.log_folder = "/tmp/dietapp/";
         }
 
+        (async () => {
+            this.fastify_port = await client.get('FASTIFY_PORT').number().catch(err =>{});
+        })();
+        if(this.fastify_port == null && process.env.FASTIFY_PORT != undefined){
+            this.fastify_port = Number(process.env.FASTIFY_PORT);
+        }
+        else{
+            this.fastify_port = 4444;
+        }
+
     }
 
     public getLog_file():string{
@@ -37,4 +49,10 @@ export class Config {
     public getLog_folder():string{
         return this.log_folder
     }
+
+    public getFastify_port():number{
+        return this.fastify_port;
+    }
 }
+
+export const configuration = new Config()
